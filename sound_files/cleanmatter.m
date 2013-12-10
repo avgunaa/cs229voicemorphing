d5e1e23cfe70fc7223d019aa66bd62ae0f796fe8
Gunaa <avgunaa@gmail.com>
2013-12-10 03:01:02 -0800

    sound update

--- sound_files/cleanmatter.m
+++ sound_files/cleanmatter.m
@@ -1,13 +1,13 @@
-num_samples = 10;
+num_samples = 50;
 
 for i=1:num_samples
-   [a,f] = wavread(strcat('words/vw_dtw',int2str(i), '.wav'));
+   [a,f] = wavread(strcat('words/aaaw_dtw',int2str(i), '.wav'));
    var = a(2000:end-1000);
    index = find(var > 0.15);
    a_new = var(index(1):index(end));
 
    
-   g = wavread(strcat('words/ggw_dtw',int2str(i), '.wav'));
+   g = wavread(strcat('words/gggw_dtw',int2str(i), '.wav'));
    var = g(2000:end-1000);
 %   index = find(var > 0.1);
    g_new = var(index(1):index(end));
@@ -17,8 +17,8 @@ for i=1:num_samples
    %a_new = a_new(1:m);
    %g_new = g_new(1:m);
    
-   wavwrite(a_new, f, strcat('words/vw_dtw_c', int2str(i),'.wav'));
-   wavwrite(g_new, f, strcat('words/ggw_dtw_c', int2str(i), '.wav'));
+   wavwrite(a_new, f, strcat('words/aaaw_dtw_c', int2str(i),'.wav'));
+   wavwrite(g_new, f, strcat('words/gggw_dtw_c', int2str(i), '.wav'));
    
    
    
--- sound_files/create_dtw.m
+++ sound_files/create_dtw.m
@@ -1,8 +1,8 @@
-for i=1:10
-    file1 = strcat('words/vw', int2str(i), '.wav');
-    file2 = strcat('words/ggw', int2str(i), '.wav');
+for i=1:50
+    file1 = strcat('words/aaaw', int2str(i), '.wav');
+    file2 = strcat('words/gggw', int2str(i), '.wav');
     [aw_dtw, gw_dtw, f] = kultmatter(file1, file2);
     size(aw_dtw)
-    wavwrite(aw_dtw, f, strcat('words/vw_dtw', int2str(i),'.wav'));
-    wavwrite(gw_dtw, f, strcat('words/ggw_dtw', int2str(i), '.wav'));
+    wavwrite(aw_dtw, f, strcat('words/aaaw_dtw', int2str(i),'.wav'));
+    wavwrite(gw_dtw, f, strcat('words/gggw_dtw', int2str(i), '.wav'));
 end
